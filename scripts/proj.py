a86f5b485217a3f380584eb53c26fc8d3e619b3d
Christopher Medlin <christopherjmedlin@gmail.com>
2018-12-11 12:46:23 -0800

    fix scripts symlink

---
+++ scripts/proj.py
@@ -0,0 +1,29 @@
+from PyInquirer import style_from_dict, Token, prompt, Separator
+import os
+
+
+def get_project_dirs(base_dir):
+    directories = []
+    for directory in os.listdir(base_dir):
+        tmux = os.path.join(base_dir, directory) + "/.tmuxinator.yml"
+        if os.path.isfile(tmux):
+            directories.append(directory)
+
+    return directories
+
+
+def start_tmux(directory):
+    os.system("cd {} && tmuxinator".format(directory)) 
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'project',
+        'message': 'Select project:',
+        'choices': get_project_dirs("/home/christophermedlin/Projects"),
+    },
+]
+
+if __name__ == "__main__":
+    answers = prompt(questions)
+    start_tmux("~/Projects/" + answers["project"])
